e87fe1da7cb82b9a9f7e1bc5eb30d2ee999e4ad3
Artem Panchenko <apanchenko@mirantis.com>
2016-09-12 19:57:13 +0300

    Add shellcheck to the config for tox
    Execute shell check using tox. Fixed bash
    syntax issues to make the test pass.
    
    Change-Id: Ib91618e5d69884752a3d343e57c4acc57497994d

--- docker/agent.sh
+++ docker/agent.sh
@@ -7,11 +7,11 @@ SERVICE='netchecker-service'
 ENDPOINT='8081/api/v1/agents'
 
 while : ; do
-  HOST=`hostname`
-  DATE=`date +"%Y-%m-%d %H:%M:%S"`
-  RESOLV=`cat /etc/resolv.conf`
-  NSLOOKUP=`nslookup $SERVICE`
-  IPS=`ip -4 a | grep inet`
+  HOST=$(hostname)
+  DATE=$(date +"%Y-%m-%d %H:%M:%S")
+  RESOLV=$(cat /etc/resolv.conf)
+  NSLOOKUP=$(nslookup $SERVICE)
+  IPS=$(ip -4 a | grep inet)
   PRE_DATA=$(cat <<EOF
 {
   "podname": "$MY_POD_NAME",
@@ -23,7 +23,7 @@ while : ; do
 }
 EOF
 )
-  DATA=`echo $PRE_DATA | tr '\n' ' '`
-  curl -i -s --connect-timeout 5 -H 'Content-Type: application/json' -X POST -d "$DATA" http://$SERVICE:$ENDPOINT/$MY_POD_NAME
+  DATA=$(echo "$PRE_DATA" | tr '\n' ' ')
+  curl -i -s --connect-timeout 5 -H 'Content-Type: application/json' -X POST -d "$DATA" "http://$SERVICE:$ENDPOINT/$MY_POD_NAME"
   sleep 60
 done
